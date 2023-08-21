b3f1b1b6d16bfa34a6ad57fd171b14f634df1309
shaunprice <shaundnz@gmail.com>
2023-08-21 17:51:20 +1200

    Updated main.ts cors for FE e2e tests

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MikroORM } from '@mikro-orm/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://127.0.0.1:5173',
+      'http://localhost:5173',
+      'http://127.0.0.1:4173',
+      'http://localhost:4173',
+    ],
+  });
+  app.useGlobalPipes(new ValidationPipe());
   await app.get(MikroORM).getSchemaGenerator().ensureDatabase();
   await app.get(MikroORM).getSchemaGenerator().updateSchema();
   await app.listen(3000);
