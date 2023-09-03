387b2b37306e1fcf191f064de0d853482d792fd1
shaunprice <shaundnz@gmail.com>
2023-09-03 18:29:32 +1200

    Created custom validators for unique username and matching passwords

---
+++ src/contracts/validators/match.decorator.ts
@@ -0,0 +1,40 @@
+import { ClassConstructor } from 'class-transformer';
+import {
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  registerDecorator,
+} from 'class-validator';
+
+export const Match = <T>(
+  type: ClassConstructor<T>,
+  property: (o: T) => any,
+  validationOptions?: ValidationOptions,
+) => {
+  return (object: any, propertyName: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: MatchConstraint,
+    });
+  };
+};
+
+@ValidatorConstraint({ name: 'Match' })
+export class MatchConstraint implements ValidatorConstraintInterface {
+  validate(value: any, args: ValidationArguments) {
+    const [fn] = args.constraints;
+    return fn(args.object) === value;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    const [constraintProperty]: ((o: any) => any)[] = args.constraints;
+    const compareProperty = constraintProperty
+      .toString()
+      .slice(constraintProperty.toString().indexOf('.') + 1);
+    return `${compareProperty} and ${args.property} does not match`;
+  }
+}
---
+++ src/contracts/validators/uniqueUsername.decorator.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+  ValidationOptions,
+  registerDecorator,
+} from 'class-validator';
+import { AuthService } from '../../modules/auth/auth.service';
+
+export const IsUniqueUsername = (validationOptions?: ValidationOptions) => {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsUniqueUsernameConstraint,
+    });
+  };
+};
+
+@ValidatorConstraint({ name: 'IsUniqueUsername', async: true })
+@Injectable()
+export class IsUniqueUsernameConstraint
+  implements ValidatorConstraintInterface
+{
+  constructor(private readonly authService: AuthService) {}
+
+  async validate(username: string) {
+    const user = await this.authService.getUser(username);
+    if (user === null) {
+      return true;
+    }
+    return false;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `Username ${args.value} already exists`;
+  }
+}
