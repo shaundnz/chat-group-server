45a643c67776fac3b960f65f5c658c861f981a17
shaunprice <shaundnz@gmail.com>
2023-09-04 20:52:22 +1200

    Set username property to unique

--- src/database/entities/User.ts
+++ src/database/entities/User.ts
@@ -1,9 +1,10 @@
-import { Entity, Property } from '@mikro-orm/core';
+import { Entity, Property, Unique } from '@mikro-orm/core';
 import { CustomBaseEntity } from './CustomBaseEntity';
 
 @Entity()
 export class User extends CustomBaseEntity {
   @Property()
+  @Unique()
   username: string;
 
   @Property()
