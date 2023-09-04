7b25a2dda43a9de0c606ca751fc5f3b5c1d4cb9b
shaunprice <shaundnz@gmail.com>
2023-09-04 21:06:26 +1200

    Added local and jwt strategies

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,20 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { UserDto } from 'src/contracts';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getOrThrow<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any): Promise<UserDto> {
+    return { id: payload.sub, username: payload.username };
+  }
+}
