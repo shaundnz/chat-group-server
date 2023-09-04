520ee00e4aa2bfa51d41e1deaf7e041be9aba811
shaunprice <shaundnz@gmail.com>
2023-09-04 20:53:42 +1200

    Added auth token to channel e2e tests

--- test/channels.e2e-spec.ts
+++ test/channels.e2e-spec.ts
@@ -1,29 +1,22 @@
 import * as request from 'supertest';
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-import { ChannelsModule } from '../src/modules/channels/channels.module';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { INestApplication } from '@nestjs/common';
 import { MikroORM } from '@mikro-orm/core';
 import { ChannelsTestDatabaseSeeder } from '../src/database/seeders';
+import { AppModule } from '../src/app.module';
+import { setupGlobalValidationPipe } from '../src/setup';
 
 describe('channels', () => {
   let app: INestApplication;
+  let token = '';
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        ChannelsModule,
-        MikroOrmModule.forRoot({
-          entities: ['./dist/database/entities'],
-          entitiesTs: ['./src/database/entities'],
-          dbName: ':memory:',
-          type: 'sqlite',
-        }),
-      ],
+      imports: [AppModule],
     }).compile();
 
     app = module.createNestApplication();
-    app.useGlobalPipes(new ValidationPipe());
+    setupGlobalValidationPipe(app);
 
     // Setup the database
     const seeder = app.get(MikroORM).getSeeder();
@@ -31,6 +24,24 @@ describe('channels', () => {
     await seeder.seed(ChannelsTestDatabaseSeeder);
 
     await app.init();
+
+    await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        username: 'userOne',
+        password: 'Password1!',
+        confirmPassword: 'Password1!',
+      })
+      .expect(201);
+    const loggedInUser = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        username: 'userOne',
+        password: 'Password1!',
+      })
+      .expect(200);
+
+    token = loggedInUser.body.accessToken;
   });
 
   afterAll(async () => {
@@ -38,7 +49,10 @@ describe('channels', () => {
   });
 
   it('GET /channels gets channels', async () => {
-    const res = await request(app.getHttpServer()).get('/channels').expect(200);
+    const res = await request(app.getHttpServer())
+      .get('/channels')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
     expect(res.body).toHaveLength(1);
     expect(res.body[0].title).toBe('Welcome');
     expect(res.body[0].description).toBe(
@@ -50,6 +64,7 @@ describe('channels', () => {
   it('GET /channels/default gets the default channel', async () => {
     const res = await request(app.getHttpServer())
       .get('/channels/default')
+      .set('Authorization', `Bearer ${token}`)
       .expect(200);
     expect(res.body.title).toBe('Welcome');
     expect(res.body.description).toBe(
@@ -59,26 +74,37 @@ describe('channels', () => {
   });
 
   it('GET /channels/:id gets channel by id', async () => {
-    const res = await request(app.getHttpServer()).get('/channels').expect(200);
+    const res = await request(app.getHttpServer())
+      .get('/channels')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
     await request(app.getHttpServer())
       .get(`/channels/${res.body[0].id}`)
+      .set('Authorization', `Bearer ${token}`)
       .expect(200);
   });
 
   it('GET /channels/:id returns 404 if channel does not exist', async () => {
-    await request(app.getHttpServer()).get('/channels/123').expect(404);
+    await request(app.getHttpServer())
+      .get('/channels/123')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(404);
   });
 
   it('POST /channels creates a new channel', async () => {
     const newChannelRes = await request(app.getHttpServer())
       .post('/channels')
+      .set('Authorization', `Bearer ${token}`)
       .send({ title: 'new channel', description: 'new channel description' })
       .expect(201);
 
     expect(newChannelRes.body.title).toBe('new channel');
     expect(newChannelRes.body.description).toBe('new channel description');
 
-    const res = await request(app.getHttpServer()).get('/channels').expect(200);
+    const res = await request(app.getHttpServer())
+      .get('/channels')
+      .set('Authorization', `Bearer ${token}`)
+      .expect(200);
     expect(res.body).toHaveLength(2);
   });
 
@@ -90,6 +116,10 @@ describe('channels', () => {
     { title: 'title', description: '' },
     { title: '', description: 'description' },
   ])('POST /channels throws validation errors for bad data', async (body) => {
-    await request(app.getHttpServer()).post('/channels').send(body).expect(400);
+    await request(app.getHttpServer())
+      .post('/channels')
+      .set('Authorization', `Bearer ${token}`)
+      .send(body)
+      .expect(400);
   });
 });
