6cc69109aa4ac9500bc84eb18de3410851921dcc
shaunprice <shaundnz@gmail.com>
2023-09-23 16:52:35 +1200

    Moved mikro config into promise

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,14 +1,21 @@
 import { Options } from '@mikro-orm/core';
+import 'dotenv/config';
 
-const config: Options = {
-  entities: ['./dist/database/entities'],
-  entitiesTs: ['./src/database/entities'],
-  dbName: 'chat-app-db.sqlite3',
-  type: 'sqlite',
-  seeder: {
-    path: 'dist/database/seeders',
-    pathTs: 'src/database/seeders',
-  },
+export default async () => {
+  const config: Options = {
+    entities: ['./dist/database/entities'],
+    entitiesTs: ['./src/database/entities'],
+    dbName: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT as string, 10),
+    type: 'postgresql',
+    seeder: {
+      path: 'dist/database/seeders',
+      pathTs: 'src/database/seeders',
+    },
+  };
+
+  return config;
 };
-
-export default config;
